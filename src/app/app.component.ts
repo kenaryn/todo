a644265f9b6714783900f69f3d2d13d6eaec413e
Aurélien Plazzotta <aurelien.plazzotta@tutanota.com>
2024-07-28 18:35:47 +0200

    Reverted back to recommanded standalone components mechanism in stead of using NgModules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,25 @@
 import { Component } from '@angular/core';
 import { TodoList } from "./todoList";
 import { TodoItem } from "./todoItem";
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 // Provides metadata about a class (i.e. config info).
 @Component({
   selector: 'app-root', // CSS selector that matches HTML tag to which the component will be applied
   standalone: true,
+  imports: [
+    FormsModule,
+    MatButtonModule, MatToolbarModule, MatIconModule, MatBadgeModule,
+    MatTableModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatSlideToggleModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { appConfig } from './app/app.config';
-// import { AppComponent } from './app/app.component';
-
-// bootstrapApplication(AppComponent, appConfig)
-//   .catch((err) => console.error(err));
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+// Entry point for an Angular app. It must be bootstrapped properly by either a component or a module.
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { AppModule } from './app/app.module';
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch((err) => console.error(err));
